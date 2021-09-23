e981dd2c885e15672e589736a4b5f2b2675eae46
Tron51226 <70756241+Tron51226@users.noreply.github.com>
2021-09-23 15:44:03 +0530

    Logical A* completed
    A* is now working, graphical visualization yet to be added

--- AStarVis/Source.cpp
+++ AStarVis/Source.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <vector>
+#include <iterator>
+#include <chrono>
 #include <stb/stb_image.h>
 
 using namespace std;
@@ -80,11 +83,55 @@ void cleanup(unsigned char* img, cell* cellData) {
 bool isValid(int i, int j, int w, int h, cell* mat) {
 	bool flag = false;
 	if (i >= 0 && i < h) {
-		if (j >= 0 && j < w && (mat + (i*w + j))->retType()!=cellType::wall) {
+		if (j >= 0 && j < w && ((mat + (i*w + j))->retType()==cellType::walkable || (mat + (i * w + j))->retType() == cellType::considered || (mat + (i * w + j))->retType() == cellType::target)) {
 			flag = true;
 		}
 	}
 	return flag;
+}
+
+void findNextCell(cell* mat, int width, int height, int &nI, int &nJ) {
+	//first find min f cost
+	double mF;
+	mF = (mat + (0 * width + 0))->f;
+	for (int i = 0; i < width; i++) {
+		for (int j = 0; j < height; j++) {
+			if ((mat + (i * width + j))->f < mF && (mat + (i * width + j))->retType() == cellType::considered) {
+				mF = (mat + (i * width + j))->f;
+				cout << "\nAdded " << i << " " << j;
+			}
+		}
+	}
+
+	//find all instances of cells with f costs equal to the minimum f values
+	vector<pair<int,int>> minPoints;
+	for (int i = 0; i < height; i++) {
+		for (int j = 0; j < width; j++) {
+			if ((mat + (i * width + j))->f == mF) {
+				minPoints.push_back(make_pair(i,j));
+				nI = i;
+				nJ = j;
+			}
+		}
+	}
+	//set nI and nJ
+
+	//find the cell with the lowest h cost
+	double minH;
+	int mI, mJ;
+	minH = (mat + (minPoints[0].first*width + minPoints[0].second))->h;
+	mI = minPoints[0].first;
+	mJ = minPoints[0].second;
+	for (int k = 0; k<minPoints.size(); k++) {
+		if ((mat + (minPoints[k].first * width + minPoints[k].second))->h < minH) {
+			minH = (mat + (minPoints[k].first * width + minPoints[k].second))->h;
+			mI = minPoints[k].first;
+			mJ = minPoints[k].second;
+		}
+	}
+	nI = mI;
+	nJ = mJ;
+	minPoints.clear();
 }
 
 void aStarSearch(cell* mat, int sI, int sJ, int eI, int eJ,int w, int h) {
@@ -94,7 +141,6 @@ void aStarSearch(cell* mat, int sI, int sJ, int eI, int eJ,int w, int h) {
 	(mat + (sI * w + sJ))->setParent(sI,sJ);
 
 	bool foundGoal = false;
-	bool* visited = new bool[w*h];
 	int numVisited = 0;
 
 	int cI, cJ;
@@ -103,10 +149,10 @@ void aStarSearch(cell* mat, int sI, int sJ, int eI, int eJ,int w, int h) {
 
 	//continue here
 	//iterate till goal found, or all cells visited
-	for (;!foundGoal && numVisited<w*h;) {
+	for (;!foundGoal && numVisited< (w*h);) {
 		//set the current cell as visited/closed
 		(mat + (cI * w + cJ))->setType(cellType::visited);
-		*(visited + (cI * w + cJ)) = true;
+		(mat + (cI*w+cJ))->setFGH(FLT_MAX,FLT_MAX,FLT_MAX);
 		numVisited++;
 
 		//check if this is the goal
@@ -249,13 +295,18 @@ void aStarSearch(cell* mat, int sI, int sJ, int eI, int eJ,int w, int h) {
 		//the node visited next will be the one with the least f cost
 		//if cells have the same f cost, then pick the one with the lower h cost
 		//if both the f cost and h cost are the same, pick any one of them arbitrarily
-
+		int nI, nJ;
+		findNextCell(mat,w,h,nI,nJ);
+		//change to the chosen cell
+		cI = nI;
+		cJ = nJ;
 	}
 }
 
 int main() {
+	auto beginTime = std::chrono::high_resolution_clock::now();
 	int width, height, numChannels;
-	unsigned char* imgData = stbi_load("Maps/testMap16.png", &width, &height, &numChannels, 0);
+	unsigned char* imgData = stbi_load("Maps/TestMap128.png", &width, &height, &numChannels, 0);
 
 	cout << "Size of the image: " << width << "x" << height<<endl;
 
@@ -271,7 +322,7 @@ int main() {
 	for (int i = 0; i < width; i++) {
 		for (int j = 0; j < height; j++) {
 			unsigned char* pixelLoc = imgData + (i * width + j) * bytePerPixel;
-			cout << i<<","<<j<<":"<<(int)pixelLoc[0] << " "<< (int)pixelLoc[1] << " "<<(int)pixelLoc[2] << " "<<(int)pixelLoc[3]<<endl;
+			//cout << i<<","<<j<<":"<<(int)pixelLoc[0] << " "<< (int)pixelLoc[1] << " "<<(int)pixelLoc[2] << " "<<(int)pixelLoc[3]<<endl;
 			(mat + (i * width + j))->setData(pixelLoc[0],pixelLoc[1],pixelLoc[2],i,j,sources,targets);
 			if (pixelLoc[0] != 255 && pixelLoc[1] == 255 && pixelLoc[2] != 255) {
 				endI = i;
@@ -295,13 +346,27 @@ int main() {
 	aStarSearch(mat, startI,startJ, endI, endJ,width,height);
 
 	//debug for the map
+	cout << "\n";
 	for (int i = 0; i < width; i++) {
 		for (int j = 0; j < height; j++) {
-			cout << (int)(mat + (i*width + j))->retType()<<" ";
+			int c = (int)(mat + (i * width + j))->retType();
+			if (c == (int)cellType::visited) {
+				cout << "X";
+			}
+			else
+			if (c == (int)cellType::wall) {
+					cout << "|";
+			}
+			else {
+				cout << ".";
+			}
 		}
 		cout << endl;
 	}
 
+	auto endTime = std::chrono::high_resolution_clock::now();
+	auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(endTime - beginTime);
+	cout << "\nTotal execution time: " << elapsed.count() * 1e-9;
 	//cleanup of data and stb 
 	cleanup(imgData,mat);
 	return 0;
